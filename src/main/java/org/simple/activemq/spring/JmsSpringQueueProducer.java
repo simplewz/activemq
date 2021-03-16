d78507712d2dc224e70db7b3e42a580d2f635e89
wangzhao <13132398914@163.com>
2021-03-16 21:20:34 +0800

    修改Spring整合ActiveMQ进行队列消息的发送与接收的配置

--- src/main/java/org/simple/activemq/spring/JmsSpringQueueProducer.java
+++ src/main/java/org/simple/activemq/spring/JmsSpringQueueProducer.java
@@ -2,6 +2,7 @@ package org.simple.activemq.spring;
 
 import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,17 @@ import javax.jms.TextMessage;
  * Spring整合ActiveMQ的生产者
  */
 @Service
-public class JmsSpringProducer {
+public class JmsSpringQueueProducer {
 
     @Autowired
+    @Qualifier("jmsQueueTemplate")
     JmsTemplate jmsTemplate;
 
     public static void main(String[] args) {
         //获取Spring容器
         ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
         //本类添加了@Service注解，所以在Spring的容器中有对应单个实例对象，名称为类的全名的首字母小写
-        JmsSpringProducer producer= (JmsSpringProducer) context.getBean("jmsSpringProducer");
+        JmsSpringQueueProducer producer= (JmsSpringQueueProducer) context.getBean("jmsSpringQueueProducer");
         //利用jmsTemplate发送消息
         producer.jmsTemplate.send((session) -> {
             TextMessage textMessage = session.createTextMessage("****Spring整合ActiveMQ的Case....");
